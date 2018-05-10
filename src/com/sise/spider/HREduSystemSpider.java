20e7a2e6995dc2b4f0caec3a2c4c462f43c207fa
'guaguaguaxia' <'1030056125@qq.com'>
2018-05-10 22:58:16 +0800

    提交使用文档

--- src/com/sise/spider/HREduSystemSpider.java
+++ src/com/sise/spider/HREduSystemSpider.java
@@ -14,6 +14,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import cn.sise.utils.Utils;
+
 import com.sise.bean.Course;
 import com.sise.bean.CourseDetail;
 import com.sise.bean.Examination;
@@ -60,22 +62,26 @@ public class HREduSystemSpider {
 		return con.post();
 	}
 
-	public static StudentInfo getMyMessage(String name, String password)
+	public StudentInfo getMyMessage(String name, String password)
 			throws IOException {
-		//1.初始化一个学生信息的bean类
+		// 1.初始化一个学生信息的bean类
 		StudentInfo mes = new StudentInfo();
-		//2.获得main.jsp的页面内容
-		Document doc = HREduSystemSpider.getContent(name, password, "http://class.sise.com.cn:7001/sise/module/student_states/student_select_class/main.jsp");
-		//3.根据获得的main.jsp的内容，拼接出个人信息的访问页面
+		// 2.获得main.jsp的页面内容
+		Document doc = HREduSystemSpider
+				.getContent(
+						name,
+						password,
+						"http://class.sise.com.cn:7001/sise/module/student_states/student_select_class/main.jsp");
+		// 3.根据获得的main.jsp的内容，拼接出个人信息的访问页面
 		Element ele = doc.select("table").get(6).select("td").get(0);
 		String link = "http://class.sise.com.cn:7001"
 				+ ele.attr("onclick").split("'")[1].replace("../../../../..",
 						"");
-		//4.访问个人信息页面
+		// 4.访问个人信息页面
 		Connection con = Jsoup.connect(link).cookies(
 				HREduSystemSpider.getCookies(name, password));
 		Document mesDoc = con.post();
-		//5.用jsoup语法筛选出需要的信息，再一一放进bean对象里
+		// 5.用jsoup语法筛选出需要的信息，再一一放进bean对象里
 		Element list = mesDoc.select("table").get(2);
 		String studentNumber = list.select("td div").get(1).text().trim();
 		String sName = list.select("td div").get(2).text().trim();
@@ -102,9 +108,9 @@ public class HREduSystemSpider {
 		return mes;
 	}
 
-	public static String[][] getCourseDemo(String name, String password)
+	public String[][] getCourseDemo(String name, String password,int year,int semester)
 			throws IOException {
-		String url = "http://class.sise.com.cn:7001/sise/module/student_schedular/student_schedular.jsp?schoolyear=2017&semester=1";
+		String url = "http://class.sise.com.cn:7001/sise/module/student_schedular/student_schedular.jsp?schoolyear="+year+"&semester="+semester;
 		Connection con = Jsoup.connect(url).cookies(
 				HREduSystemSpider.getCookies(name, password));
 		Document doc = con.post();
@@ -120,11 +126,19 @@ public class HREduSystemSpider {
 		}
 		return course;
 	}
-
-	public static HashMap getAttendance(String name, String password)
+	
+	//返回考勤记录
+	public HashMap getAttendance(String name, String password)
 			throws IOException {
-		Document doc = HREduSystemSpider.getContent(name, password, "http://class.sise.com.cn:7001/sise/module/student_states/student_select_class/main.jsp");
+		//获取网页Document
+		Document doc = HREduSystemSpider
+				.getContent(
+						name,
+						password,
+						"http://class.sise.com.cn:7001/sise/module/student_states/student_select_class/main.jsp");
+		//选择指定网页元素
 		Element ele = doc.select("table").get(9).select("td").get(0);
+		//获取到指定链接
 		String link = "http://class.sise.com.cn:7001"
 				+ ele.attr("onclick").split("'")[1].replace("../../../../..",
 						"");
@@ -133,7 +147,9 @@ public class HREduSystemSpider {
 		Document attDoc = con.post();
 		Elements attTable = attDoc.select("table").get(6)
 				.select("tr.odd,tr.even");
+		//用Map存储考勤记录
 		HashMap<String, String> map = new HashMap<String, String>();
+		//循环放入
 		for (int i = 0; i < attTable.size(); i++) {
 			Element attSingle = attTable.select("tr.odd,tr.even").get(i);
 			String cName = attSingle.select("td").get(1).text();
@@ -143,39 +159,41 @@ public class HREduSystemSpider {
 		return map;
 	}
 
-	public static boolean isContains(String[] s, String keyweek) {
-		for (String s1 : s) {
-			if (s1.equals(keyweek)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	// k:哪周的课
-	public static String[][] getCourse(String name, String password, String k)
+	
+	
+	
+	//返回课程名称的字符数组，第一个维度数字为第几节课，第二个维度为星期几
+	public String[][] getCourse(String name, String password, int year,int semester,String k)
 			throws IOException {
-
-		String[][] courseDemo = HREduSystemSpider.getCourseDemo(name, password);
+		HREduSystemSpider hs = new HREduSystemSpider();
+		String[][] courseDemo = hs.getCourseDemo(name, password,year,semester);
 		String[][] course = new String[9][6];
 		int m = 1;
+		//一二层的for循环为循环二维数组，
 		for (int i = 1; i < courseDemo.length; i++) {
 			for (int j = 1; j < courseDemo[i].length; j++) {
+				/*\u00a0为网页空格$nbsp;的字符表示，因为在网页中没课不是用null表示，而是还有
+				 * 一个$nbsp;,所以需要过滤掉，空就不用进入循环了*/
 				if (!courseDemo[i][j].replace("\u00a0", "").equals("")) {
+					//如果在一个学期里，一个时间段有两门课，即前八周后八周，
+					//系统会用逗号把他们分割，所以这里需要判断一下
 					if (!courseDemo[i][j].contains(",")) {
+						//用正则表达式匹配切割出课程名称
 						String[] str = courseDemo[i][j]
 								.split("\\([a-zA-Z]{2,}\\d{0,}");
 						String[] keyReplace = courseDemo[i][j].split(" ", 2)[1]
 								.split("周")[0].split(" ");
-						if (isContains(keyReplace, k)
+						//这段逻辑我忘了，不懂的打断点慢慢看返回值自己领悟
+						if (Utils.isContains(keyReplace, k)
 								&& !courseDemo[i][j].contains(",")) {
 							course[i][j] = str[0];
 						}
 					}
+					//同上
 					if (courseDemo[i][j].contains(",")) {
 						String[] spCourse = courseDemo[i][j].split(",");
 
-						if (isContains(
+						if (Utils.isContains(
 								spCourse[0].split(" ", 2)[1].split("周")[0]
 										.split(" "),
 								k)) {
@@ -183,7 +201,7 @@ public class HREduSystemSpider {
 									.split("\\([a-zA-Z]{2,}\\d{0,}");
 							course[i][j] = strPart[0];
 						}
-						if (isContains(
+						if (Utils.isContains(
 								spCourse[1].split(" ", 2)[1].split("周")[0]
 										.split(" "),
 								k)) {
@@ -197,98 +215,17 @@ public class HREduSystemSpider {
 		}
 		return course;
 	}
-
-	public static Course[] getUsualScore(String name, String password)
-			throws IOException {
-		Document doc = getContent(
-				name,
-				password,
-				"http://class.sise.com.cn:7001/sise/module/commonresult/index.jsp?schoolyear=2017&semester=1");
-		Elements attTable = doc.select("table").get(4).select("tbody").get(1)
-				.select("tr");
-		int i = 0;
-		ArrayList<CourseDetail> list = new ArrayList();
-		Course c = new Course();
-		Course cc[] = new Course[attTable.size()];
-		for (Element e : attTable) {
-
-			if (i != 0) {
-				String id = e.select("td").get(0).select("a").first()
-						.attr("href").split("=")[1].split("&")[0];
-				c.setId(id);
-				String Coursename = e.select("td").get(0).select("a").first()
-						.text();
-				c.setName(Coursename);
-				String coursecode = e.select("td").get(0).select("span")
-						.first().text().split(" ")[0];
-				c.setCoursecode(coursecode);
-
-				String url = "http://class.sise.com.cn:7001/sise/module/commonresult/showdetails.jsp?courseid="
-						+ id + "&schoolyear=2017&semester=1";
-				Document coursedetail = getContent(name, password, url);
-				Elements ele = coursedetail.select("table tbody tbody tr");
-				int j = 0;
-				for (Element e1 : ele) {
-					if (j != 0) {
-						String[] ss = e1.text().split(" ");
-						CourseDetail cd = new CourseDetail();
-						cd.setSource(ss[0]);
-						cd.setPercent(ss[1]);
-						cd.setHighscore(ss[2]);
-						cd.setScore(ss[3]);
-						list.add(cd);
-						c.setCoursedetails(list);
-
-					}
-					j++;
-				}
-				cc[i] = c;
-			}
-
-			i++;
-		}
-
-		return cc;
-	}
-
-	public static ArrayList getPart1(String name, String password)
-			throws IOException {
-		Document doc = getContent(
-				name,
-				password,
-				"http://class.sise.com.cn:7001/sise/module/commonresult/index.jsp?schoolyear=2017&semester=1");
-		Elements attTable = doc.select("table").get(4).select("tbody").get(1)
-				.select("tr");
-
-		ArrayList<Course> list = new ArrayList<Course>();
-		int i = 0;
-		for (Element e : attTable) {
-			Course c = new Course();
-			if (i != 0) {
-				String id = e.select("td").get(0).select("a").first()
-						.attr("href").split("=")[1].split("&")[0];
-				c.setId(id);
-				String Coursename = e.select("td").get(0).select("a").first()
-						.text();
-				c.setName(Coursename);
-				String coursecode = e.select("td").get(0).select("span")
-						.first().text().split(" ")[0];
-				c.setCoursecode(coursecode);
-				list.add(c);
-			}
-			i++;
-
-		}
-		return list;
-	}
-
-	public static ArrayList getPart2(String name, String password,int year,int semester)
-			throws IOException {
-		ArrayList<Course> list = getPart1(name, password);
+	
+	public ArrayList getUsualScore(String name, String password, int year,
+			int semester) throws IOException {
+		ArrayList<Course> list = Utils.getPart1(name, password);
 		for (int i = 0; i < list.size(); i++) {
-			
+
 			String url = "http://class.sise.com.cn:7001/sise/module/commonresult/showdetails.jsp?courseid="
-					+ list.get(i).getId() + "&schoolyear="+year+"&semester="+semester;
+					+ list.get(i).getId()
+					+ "&schoolyear="
+					+ year
+					+ "&semester=" + semester;
 			Document doc = getContent(name, password, url);
 			Elements ele = doc.select("table tbody tbody tr");
 			int j = 0;
@@ -303,30 +240,24 @@ public class HREduSystemSpider {
 					cd.setScore(ss[3]);
 					list.get(i).setScore(ss[3]);
 					listDetail.add(cd);
-				      }
+				}
 				list.get(i).setCoursedetails(listDetail);
-				
+
 				j++;
 			}
-			
-			
+
 		}
 		return list;
 	}
 
-	public static String getStudentId(String name,String password) throws IOException{
-		String url = "http://class.sise.com.cn:7001/sise/module/student_states/student_select_class/main.jsp";
-		Document doc = getContent(name, password, url);
-		String str = doc.select("td table tr td").get(4).attr("onclick").split("studentid=")[1].split("'")[0];
-		return str;
-		
-	}
-	public static ArrayList<Examination> getTest(String name,String password) throws IOException{
-		String url = "http://class.sise.com.cn:7001/SISEWeb/pub/exam/studentexamAction.do?method=doMain&studentid="+getStudentId(name, password);
+	public ArrayList<Examination> getExams(String name, String password)
+			throws IOException {
+		String url = "http://class.sise.com.cn:7001/SISEWeb/pub/exam/studentexamAction.do?method=doMain&studentid="
+				+ Utils.getStudentId(name, password);
 		Document doc = getContent(name, password, url);
 		Elements ele = doc.select("table.table").select("tr.odd,tr.even");
 		ArrayList<Examination> exams = new ArrayList<Examination>();
-		for(Element e:ele){
+		for (Element e : ele) {
 			Examination exam = new Examination();
 			exam.setCoursecode(e.select("td").get(0).text());
 			exam.setCoursename(e.select("td").get(1).text());
@@ -338,58 +269,13 @@ public class HREduSystemSpider {
 			exam.setExamstatus(e.select("td").get(7).text());
 			exams.add(exam);
 		}
-		
+
 		return exams;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		String name = "1540129539";
 		String password = "19960930";
-//		ArrayList<Examination> list = getTest(name, password);
-//		for(int i = 0;i<list.size();i++){
-//			System.out.println(list.get(i).getCoursename());
-//		}
-//		ArrayList<Course> list = getPart2(name, password,2017,1);
-//		for(int i = 0;i<list.size();i++){
-//			System.out.println(list.get(i).getName());
-//			System.out.println(list.get(i).getCoursecode());
-//			System.out.println(list.get(i).getName());
-//			System.out.println(list.get(i).getScore());
-//			System.out.println(list.get(i).getCoursedetails().get(0).getSource());
-//			System.out.println(list.get(i).getCoursedetails().get(0).getPercent());
-//			System.out.println(list.get(i).getCoursedetails().get(0).getHighscore());
-//			System.out.println(list.get(i).getCoursedetails().get(0).getScore());
-//		}
-		// ArrayList<Course> list = getPart1(name, password);
-		// for(int i = 0;i<list.size();i++){
-		// }
-		// Course c = getPart1(name, password);
-		// System.out.println(c.getCoursedetails().size());
-		// for(int i = 0;i<c.getCoursedetails().size();i++){
-		// System.out.println(c.getCoursedetails().get(i).getSource());
-		// System.out.println(c.getCoursedetails().get(i).getPercent());
-		// System.out.println(c.getCoursedetails().get(i).getHighscore());
-		// System.out.println(c.getCoursedetails().get(i).getScore());
-		// }
-
-		// Document doc = getContent(name, password,
-		// "http://class.sise.com.cn:7001/sise/module/commonresult/showdetails.jsp?courseid=2800&schoolyear=2017&semester=1");
-		// System.out.println(doc);
-		// String[][] course = getCourseDemo(name, password);
-		// for(int i = 0; i < 9;i++){
-		// for(int j = 0; j < 6;j++){
-		// System.out.println(course[i][j]);
-		// }
-		// }
-
-		// HashMap map = getAttendance(name, password);
-		// for (Object key : map.keySet()) {
-		// System.out.println(key + " : " + map.get(key));
-		// }
-
-		// Document doc = getContent(name, password,
-		// "http://class.sise.com.cn:7001/sise/module/commonresult/showdetails.jsp?courseid=2648&schoolyear=2017&semester=1");
-		// System.out.println(doc.toString());
 	}
 
 }
